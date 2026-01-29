d0e379f5bd65e80e1ecf09df8a56258ba8561012
Kereszturi Balázs <diak@petrik.hu>
2026-01-29 13:48:35 +0100

    a

---
+++ test/harmadik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	Dictionary<adat, int> stat;
+	internal class harmadik
+	{
+		stat = new Dictionary<adat, int>();
+
+		
+	}
+}
