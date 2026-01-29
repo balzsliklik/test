4d7a7577604a220ffeaad1ea72ebfce5e76ae451
Kereszturi Balázs <diak@petrik.hu>
2026-01-29 13:41:10 +0100

    dsgsd

---
+++ test/adat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	internal class adat
+	{
+		private string nev;
+		private int ar;
+
+		public adat(string nev, int ar)
+		{
+			this.nev = nev;
+			this.ar = ar;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Ar { get => ar; set => ar = value; }
+	}
+}
